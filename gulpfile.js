9878eee7fa5369ecf34843d13bf26b0be7df33d2
Robin Screech <robinscreech4@gmail.com>
2018-08-27 22:30:54 +0100

    added less and watch with browsersync gfor gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,14 @@
+/**
+ *
+ * gulp less
+ * gulp watch
+ * https://css-tricks.com/gulp-for-beginners/
+ *
+ */
+
 const gulp = require('gulp');
 const less = require('gulp-less');
+const browserSync = require('browser-sync').create();
 
 var path = require('path');
  
@@ -8,10 +17,33 @@ gulp.task('less', function () {
     .pipe(less({
       paths: [ path.join(__dirname, 'less', 'includes') ]
     }))
-    .pipe(gulp.dest('./dist/css'));
+    .pipe(gulp.dest('./dist/css'))
+	.pipe(browserSync.reload({
+		stream: true
+	}))
 });
 
-gulp.task('css' function () {
-	return gulp.src('./css/*.css')
-	.pipe(gulp.dest('./dist/css'))
+
+gulp.task('browserSync', function() {
+  browserSync.init({
+    server: {
+      baseDir: 'gulp_weather_app'
+    },
+  })
 })
+
+//gulp.watch('files to watch', ['tasks', 'to', 'run'])
+
+gulp.task('watch', ['browserSync', 'less'], function (){
+  gulp.watch('./css/*.scss', ['less']); 
+  // Other watchers
+});
+
+/**
+
+	TODO:
+	- Fix the browser sync not serving file
+	- Continue through tutorial
+	- Make sure its serving up the Dist folder
+
+ */
